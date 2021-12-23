df962735b13559b38d51b24262f90f4fcf64852d
jnszkr <joszkar@gmail.com>
2021-12-23 14:05:40 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	// if the file does not exist, create it, or append to the file
+	f, err := os.OpenFile(".notes", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// read input
+	b := bytes.Buffer{}
+	b.WriteString(time.Now().Local().Format(time.RFC3339))
+	b.WriteString(" ")
+	for _, arg := range os.Args[1:] {
+		b.WriteString(arg)
+		b.WriteString(" ")
+	}
+	b.WriteString("\n")
+
+	// append input
+	_, err = f.Write(b.Bytes())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// close file
+	f.Close()
+}
