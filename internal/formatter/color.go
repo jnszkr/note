c0b7d550d47a7e97b2cf6800bc42e2ad717c682c
Oszkár Jóna <joszkar@gmail.com>
2022-01-08 00:49:07 +0100

    Moved text highlight to formatter. (#12)

--- internal/formatter/color.go
+++ internal/formatter/color.go
@@ -2,21 +2,46 @@ package formatter
 
 import (
 	"fmt"
+	"regexp"
 	"runtime"
 )
 
+type Color string
+
 var (
-	reset = "\033[0m"
-	red   = "\033[31m"
+	Reset  Color = "\033[0m"
+	Red    Color = "\033[31m"
+	Green  Color = "\033[32m"
+	Yellow Color = "\033[33m"
+	Blue   Color = "\033[34m"
+	Purple Color = "\033[35m"
+	Cyan   Color = "\033[36m"
+	Gray   Color = "\033[37m"
+	White  Color = "\033[97m"
 )
 
+var winos = false
+
 func init() {
-	if runtime.GOOS == "windows" {
-		reset = ""
-		red = ""
+	winos = runtime.GOOS == "windows"
+	if winos {
+		Reset = ""
+		Red = ""
+		Green = ""
+		Yellow = ""
+		Blue = ""
+		Purple = ""
+		Cyan = ""
+		Gray = ""
+		White = ""
 	}
 }
 
-func Red(s string) string {
-	return fmt.Sprintf("%s%s%s", red, s, reset)
+// Highlight would highlight the substring from s with the given color.
+func Highlight(s, substr string, c Color) string {
+	if winos {
+		return s
+	}
+	re := regexp.MustCompile(fmt.Sprintf("(?i)(%s)", substr))
+	return re.ReplaceAllString(s, fmt.Sprintf("%s$1%s", c, Reset))
 }
